05ab90535c98258528fdd7c79bb99f7b3a25e301
Karel Minarik <karmi@karmi.cz>
2010-11-27 17:32:30 +0100

    Added server code (reading log and streaming to websocket clients) and simple HTML GUI

--- server.js
+++ server.js
@@ -1,6 +1,24 @@
-// http://stackoverflow.com/questions/3499152/minimum-websocket-nodejs-tail-example/3646471#3646471
+// ===================================
+// `tail -f` in Node.js and WebSockets
+// ===================================
+//
+// Usage:
+// 
+// git clone git://gist.github.com/718035.git tail.js
+// cd tail.js
+// git submodule update --init
+//
+// node server.js /path/to/your.log
+//
+// Connect with browser and watch changes in the logfile
+//
+//
+require.paths.unshift('./lib/socket-io/lib/',
+                      './lib/socket.io-node/lib/');
 
-var util = require('util')
+var http    = require('http'),
+    io      = require('socket.io'),
+    fs      = require('fs');
 
 var spawn = require('child_process').spawn;
 
@@ -9,11 +27,31 @@ if (!filename) return util.puts("Usage: node <server.js> <filename>");
 
 var tail = spawn("tail", ["-f", filename]);
 
-http = require('http');
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': "text/plain;charset=UTF-8"});
+// -- Node.js Server ----------------------------------------------------------
+
+server = http.createServer(function(req, res){
+  res.writeHead(200, {'Content-Type': 'text/html'})
+  fs.readFile(__dirname + '/index.html', function(err, data){
+  	res.write(data, 'utf8');
+  	res.end();
+  });
+})
+server.listen(8000, '0.0.0.0');
+
+// -- Setup Socket.IO ---------------------------------------------------------
+
+var io = io.listen(server);
+
+io.on('connection', function(client){
+  console.log('Client connected');
+  client.send( { filename : filename } );
+
   tail.stdout.on("data", function (data) {
-    res.write(data);
+    console.log(data.toString('utf-8'))
+    client.send( { tail : data.toString('utf-8') } )
   }); 
-}).listen(8000);
+
+});
+
+console.log('Server running at http://0.0.0.0:8000/, connect with a browser to see tail output');
