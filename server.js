d80afd0b305c165efa49bba632344e2713cd2421
Aditya Yadav <aditya@netroy.in>
2011-03-13 08:48:00 +0530

    added support to select log files from /var/log

--- server.js
+++ server.js
@@ -6,42 +6,66 @@ var http    = require('http'),
     fs      = require('fs');
 
 var backlog_size = 2000;
-var filename = process.ARGV[2];
-if (!filename) return util.puts("Usage: node <server.js> <filename>");
+var log_dir = "/var/log/";
+var logs = [];
+// look up the dir for logs
+fs.readdir(log_dir, function(err,files){
+  if(err) throw err;
+  files = Array.prototype.sort.apply(files,[]);
+  for(var file in files){
+    file = files[file];
+    if(fs.statSync(log_dir+file).isFile()) logs.push(file);
+  }
+});
 
 // -- Node.js HTTP Server ----------------------------------------------------------
 server = http.createServer(function(req, res){
   res.writeHead(200, {'Content-Type': 'text/html'})
   fs.readFile(__dirname + '/index.html', function(err, data){
-  	res.write(data, 'utf8');
-  	res.end();
+    res.write(data, 'utf8');
+    res.end();
   });
 })
-server.listen(8000, '0.0.0.0');
+server.listen(8000);
 
 // -- Setup Socket.IO ---------------------------------------------------------
 var socket = io.listen(server);
 socket.on('connection', function(client){
-  client.send( { filename : filename } );
-  fs.stat(filename,function(err,stats){
-    if (err) throw err;
-    var start = (stats.size > backlog_size)?(stats.size - backlog_size):0;
-    var stream = fs.createReadStream(filename,{start:start, end:stats.size});
-    stream.addListener("data", function(lines){
-      lines = lines.toString('utf-8');
-      lines = lines.slice(lines.indexOf("\n")+1).split("\n");
-      client.send({ tail : lines});
-    });
+  var filename;
+  client.send( { logs : logs } );
+  client.on("message",function(message){
+    if(message.log){
+      // Stop watching the last file and send the new one
+      fs.unwatchFile(filename);
+      filename = log_dir + message.log;
+      client.send({filename: filename});
+      // send some back log
+      fs.stat(filename,function(err,stats){
+        if (err) throw err;
+        var start = (stats.size > backlog_size)?(stats.size - backlog_size):0;
+        var stream = fs.createReadStream(filename,{start:start, end:stats.size});
+        stream.addListener("data", function(lines){
+          lines = lines.toString('utf-8');
+          lines = lines.slice(lines.indexOf("\n")+1).split("\n");
+          client.send({ tail : lines});
+        });
+      });
+
+      // watch the file now
+      fs.watchFile(filename, function(curr, prev) {
+        if(prev.size > curr.size) return {clear:true};
+        var stream = fs.createReadStream(filename, { start: prev.size, end: curr.size});
+        stream.addListener("data", function(lines) {
+          client.send({ tail : lines.toString('utf-8').split("\n") });
+        });
+      });
+
+      // stop watching the file
+      client.on("disconnect",function(){
+        fs.unwatchFile(filename);
+      });
+    }
   });
 });
 
-// watch the file now
-fs.watchFile(filename, function(curr, prev) {
-  if(prev.size > curr.size) return {clear:true};
-  var stream = fs.createReadStream(filename, { start: prev.size, end: curr.size});
-  stream.addListener("data", function(lines) {
-    socket.broadcast({ tail : lines.toString('utf-8').split("\n") });
-  });
-});
-
-console.log('Server running at http://0.0.0.0:8000/, connect with a browser to see tail output');
+console.log('Log Server running now at http://[HOSTNAME]:8000/ in your browser');
